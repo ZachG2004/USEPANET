0292a1f4b715bd1bbd6feee50a3120b6c59880e7
ZachG2004 <GitHub@ZachG.io>
2025-02-21 18:35:31 -0500

    Moving handler to library folder and re-establishing assignment script location

---
+++ Homework Assignment 04/homework4run.cpp
@@ -0,0 +1,16 @@
+/////////////////////////////////////////
+////       Zachary A. Gioppo        ////
+//// Spring 2025 ENE422 Hydraulics ////
+////   Common Operations Parser   ////
+/////////////////////////////////////
+
+// namespace and inclusions established
+#include <iostream>
+#include <epanet2_2.h>
+#include </include/handlerForEPANET.cpp>
+using namespace std;
+
+int main(void){
+  std::out("compiled!");
+
+}
