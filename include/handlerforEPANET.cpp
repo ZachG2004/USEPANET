f6d007694ca7c1dde767a22032d6dd99b6293583
ZachG2004 <GitHub@ZachG.io>
2025-02-21 22:48:09 -0500

    Modification of Hydraulics Handler, Saving now so I can reference differences as I develop other

--- include/handlerforEPANET.cpp
+++ include/handlerforEPANET.cpp
@@ -20,37 +20,60 @@ void runSetup(EN_Project pp, int *flowUnit, int *headFormula, char *reportFile, 
   char errmsg[EN_MAXMSG + 1];
 
   // Run these commands to setup EN_Project
-  ERRCODE(EN_createproject(pp));
-  ERRCODE();
+  ERRCODE(EN_createproject(&pp));
+  if (outFile != "") { ERRCODE(EN_init(pp, reportFile.append(".rpt"), outFile.append(".out"), flowUnit, headFormula)); }
+  else { ERRCODE(EN_init(pp, reportFile.append(".rpt"), "", flowUnit, headFormula)); }
 
-}
-
-// Establish Error Handler to generate Hydraulics Report, while parsing errors along the way
-void runHydraulics(EN_Project pp, char *reportFile, char *inputFile = "", char runType = "noinput", int saveInput = 1) {
-  if (saveInput == 1 && inputFile != "") { continue; }
-    else if (saveInput == 1 && inputFile == "") { printf("In order to save Input, please specify an input file name \n"); return 1; }
-    else if (saveInput == 0) { printf("Continuing without saving InputFile"); continue; }
-    else return ("saveInput must be '0' or '1' (inputted: %d) | runType must be 'input' or 'noinput' (inputted: %s)", saveInput, runType);
-
-  // Establish code number variable and counter/msg
-  int errcode = 0;  
-  char errmsg[EN_MAXMSG + 1];
-
-  // If running an InputFile; otherwise assume project was initalized
-  if (runType == "input") { ERRCODE(EN_open(pp, inputFile.append(".inp"), reportFile.append(".rpt"), "")); continue; }
-  else { printf("Assuming EN_init function was ran to initalize Project. \n"); continue; }
-  // Hydraulic Analysis
-  ERRCODE(EN_solveH(pp));
-  ERRCODE(EN_saveH(pp));
-  ERRCODE(EN_report(pp));
-
-    // If a saveName is specified for input file, run savefile
-  if (saveInput == 1 && runType == "noinput") { ERRCODE(EN_saveinpfile(pp, inpSaveName.append(".inp"))); }
-  else if (saveInput == 1 && runType == "input") { auto time = std::chrono::system_clock::now(); ERRCODE(EN_saveinpfile(pp, inpSaveName.append("[Ran: %s]", time))); }
-  else continue;
   // Searcj the error message code for its explanation
   EN_geterror(errcode, errmsg, EN_MAXMSG);
 
   // Return Error Code with message and appropriate information
-  if (errode) { printf("Error Code [%d], with Message: \n%s\n", errcode, errmsg); }
+  if (errcode) { printf("Error Code [%d], with Message: \n%s\n", errcode, errmsg); }
+}
+
+// Establish Error Handler to generate Hydraulics Report, while parsing errors along the way
+
+void runHydraulics(EN_Project pp, std::string reportFile, std::string inputFile = "", std::string runType = "noinput", int saveInput = 1) {
+  // Validate saveInput and runType
+  if (saveInput != 0 && saveInput != 1) { printf("saveInput must be '0' or '1' (inputted: %d)\n", saveInput); return; }
+  if (runType != "input" && runType != "noinput") { printf("runType must be 'input' or 'noinput' (inputted: %s)\n", runType.c_str()); return; }
+
+  if (saveInput == 1 && inputFile.empty()) { printf("In order to save Input, please specify an input file name.\n"); return; }
+  else if (saveInput == 0) { printf("Continuing without saving InputFile.\n"); }
+
+  // Establish error code and message buffer
+  int errcode = 0;
+  char errmsg[EN_MAXMSG + 1];
+
+  // Open file if running from input
+  if (runType == "input") {
+      std::string fullInputFile = inputFile + ".inp";
+      std::string fullReportFile = reportFile + ".rpt";
+      errcode = EN_open(pp, fullInputFile.c_str(), fullReportFile.c_str(), "");
+  }
+  else { printf("Assuming EN_init function was run to initialize Project.\n") }
+
+  // Run hydraulic analysis
+  ERRCODE(EN_solveH(pp));
+  ERRCODE(EN_saveH(pp));
+  ERRCODE(EN_report(pp));
+
+  // Save input file if needed
+  if (saveInput == 1) {
+      std::string saveName;
+      if (runType == "noinput") {
+          saveName = inputFile + ".inp";
+      }
+      else {
+          auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+          saveName = inputFile + "[Ran: " + std::to_string(time) + "].inp";
+      }
+      errcode = EN_saveinpfile(pp, saveName.c_str());
+  }
+
+  // Retrieve error message if any
+  if (errcode) {
+      EN_geterror(errcode, errmsg, EN_MAXMSG);
+      printf("Error Code [%d], Message: \n%s\n", errcode, errmsg);
+  }
 }
