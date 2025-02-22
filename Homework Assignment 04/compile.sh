2bdf8d79f4a6ea18ab863d1d1f7bf6bda39b3ef2
ZachG2004 <GitHub@ZachG.io>
2025-02-22 01:31:51 -0500

    Added Buildfolder

--- Homework Assignment 04/compile.sh
+++ Homework Assignment 04/compile.sh
@@ -1,7 +1,9 @@
 #/bin/zsh
+cd ~/Documents/USEPANET/Homework\ Assignment\ 04/builds
+
 g++ \
 -o hw4net_cmp \
 ~/Documents/USEPANET/Homework\ Assignment\ 04/homework4run.cpp \
 ~/Documents/USEPANET/include/handlerforEPANET.cpp \
 -L/Users/zachfrost/Applications/EPANET/build/lib \
--lepanet2 
+-lepanet2
